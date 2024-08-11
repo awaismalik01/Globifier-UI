a2094c5f4b6d38d86d17b01a54360a7137567718
Awais Ahmed Malik <mawais_malik@hotmail.com>
2024-08-10 21:51:44 -0400

    Project Setup

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
